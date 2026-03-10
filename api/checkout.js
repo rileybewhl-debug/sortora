b7f279c78bd3ddb41c3ca4aa1780bdd2b73192ec
rileybewhl-debug <rileybewhl@gmail.com>
2026-03-10 07:03:44 -0700

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -22,8 +22,8 @@ export default async function handler(req, res) {
       quantity: 1,
     }],
     mode: 'payment',
-    success_url: 'https://sortora.vercel.app/browse.html?payment=success',
-    cancel_url: 'https://sortora.vercel.app/browse.html?payment=cancelled',
+success_url: 'https://sortora.com/browse.html?payment=success',
+cancel_url: 'https://sortora.com/browse.html?payment=cancelled',
     metadata: { sessionId, customerId }
   });
 
